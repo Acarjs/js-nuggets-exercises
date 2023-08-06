8f63a7d3e934e781e438aad430aa4502d433fbb3
Anil Acar <ostravba@gmail.com>
2023-08-07 00:03:18 +0200

    array from part-1

--- 11-array-from/app.js
+++ 11-array-from/app.js
@@ -1,1 +1,15 @@
+// Array.from - NOT ON PROTOTYPE
 
+// from - returns Array Object from an object
+// with a length property or an iterable object
+// from - turns array-like/ish into array - string, nodeList, Set
+
+const udemy = 'udemy'
+
+// console.log(Array.from(udemy))
+
+const text = document.querySelectorAll('.text') //array methods doesn't work on nodelist
+console.log(text)
+
+const newText = Array.from(text).find((item) => item.textContent === 'person')
+console.log(newText)
