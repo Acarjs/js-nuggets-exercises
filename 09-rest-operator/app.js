b7f0ff5b0adcafd1ca95b8c497ea80015d76b44b
Anil Acar <ostravba@gmail.com>
2023-08-06 18:37:45 +0200

    rest operator

--- 09-rest-operator/app.js
+++ 09-rest-operator/app.js
@@ -6,10 +6,29 @@
 
 // arrays
 const fruits = ['apple', 'orange', 'lemon', 'banana', 'pear']
-const [first, ...rest] = fruits //this is array destructuring
-console.log(first, rest)
+const [first, second, ...restOfTheFruits] = fruits //this is array destructuring
+console.log(first, second, restOfTheFruits) //apple orange ['lemon', 'banana', 'pear']
+const specificFruit = restOfTheFruits.find((fruit) => fruit === 'lemon')
+console.log(specificFruit)
 
 // objects
 const person = { name: 'john', lastName: 'smith', job: 'developer' }
-const { name } = person //this is object destructuring
-console.log(name)
+const { name, ...rest } = person //this is object destructuring
+
+console.log(name, rest) //john {lastName: 'smith', job: 'developer'}
+
+// functions
+const getAverage = (name, ...scores) => {
+  console.log(name)
+  console.log(scores)
+  const average =
+    scores.reduce((total, item) => {
+      return (total = total + item)
+    }, 0) / scores.length
+
+  console.log(average)
+}
+
+// getAverage(person.name, 87, 98, 100, 34)
+const testScores = [25, 46, 78, 12]
+getAverage(person.name, ...testScores)
