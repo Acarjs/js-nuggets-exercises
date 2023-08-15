426d7b3854b793b6cdea29e5b89d421d76bf2bc2
Anil Acar <ostravba@gmail.com>
2023-08-15 20:40:48 +0200

    promise example setup

---
+++ 16-promise-example/app.js
@@ -0,0 +1,7 @@
+//  Javascript Nuggets - Promises Example
+// .first - after 1s first red;
+// .second - after 3s second blue; 4s
+// .third - after 2s third green; 6s
+// IN SEQUENCE !!!!
+
+const btn = document.querySelector('.btn')
