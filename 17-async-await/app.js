eedccbf498b000c1373af5d89e8c7967da8441cb
Anil Acar <ostravba@gmail.com>
2023-09-02 23:37:35 +0200

    async-await setup

---
+++ 17-async-await/app.js
@@ -0,0 +1,17 @@
+//  Javascript Nuggets
+// async / await
+// async must be present, always returns a promise
+// await waits till promise is settled
+// error handling - try/catch block
+
+const users = [
+  { id: 1, name: 'john' },
+  { id: 2, name: 'susan' },
+  { id: 3, name: 'anna' },
+]
+
+const articles = [
+  { userId: 1, articles: ['one', 'two', 'three'] },
+  { userId: 2, articles: ['four', 'five'] },
+  { userId: 3, articles: ['six', 'seven', 'eight', 'nine'] },
+]
